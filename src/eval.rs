1a5fb9d17b93a2c5ba5f487979ffc7db5cbfc3be
Bruno Andreotti <bandreotti@protonmail.com>
2020-03-24 19:57:47 -0300

    Add evaluation of prefix expressions

--- src/eval.rs
+++ src/eval.rs
@@ -1,11 +1,18 @@
+// @TODO: Add proper error handling: currently, all evaluation functions panic instead of returning
+// errors
 use crate::ast::*;
 use crate::object::*;
+use crate::token::Token;
 
 // @WIP
 pub fn eval_expression(expression: Expression) -> Object {
     match expression {
         Expression::IntLiteral(i) => Object::Integer(i),
         Expression::Boolean(b) => Object::Boolean(b),
+        Expression::PrefixExpression(tk, e) => {
+            let right_side = eval_expression(*e);
+            eval_prefix_operator(tk, right_side)
+        }
         _ => panic!(),
     }
 }
@@ -25,9 +32,19 @@ pub fn eval_statement(statement: Statement) -> Object {
     }
 }
 
+// @WIP
+fn eval_prefix_operator(operator: Token, right: Object) -> Object {
+    match (operator, right) {
+        (Token::Bang, Object::Boolean(b)) => Object::Boolean(!b),
+        (Token::Minus, Object::Integer(i)) => Object::Integer(-i),
+        _ => panic!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use Object::*;
 
     fn assert_eval(input: &str, expected: &[Object]) {
         use crate::lexer::Lexer;
@@ -42,29 +59,23 @@ mod tests {
             .for_each(|(got, exp)| assert_eq!(&got, exp)); // Compare with expected
     }
 
-
     #[test]
     fn test_eval_int_literal() {
-        assert_eq!(eval_expression(Expression::IntLiteral(1)), Object::Integer(1));
-        assert_eq!(eval_expression(Expression::IntLiteral(2)), Object::Integer(2));
-        assert_eq!(eval_expression(Expression::IntLiteral(3)), Object::Integer(3));
+        assert_eq!(eval_expression(Expression::IntLiteral(1)), Integer(1));
+        assert_eq!(eval_expression(Expression::IntLiteral(2)), Integer(2));
+        assert_eq!(eval_expression(Expression::IntLiteral(3)), Integer(3));
     }
 
     #[test]
     fn test_eval_bool_literal() {
-        assert_eq!(eval_expression(Expression::Boolean(true)), Object::Boolean(true));
-        assert_eq!(eval_expression(Expression::Boolean(false)), Object::Boolean(false));
+        assert_eq!(eval_expression(Expression::Boolean(true)), Boolean(true));
+        assert_eq!(eval_expression(Expression::Boolean(false)), Boolean(false));
     }
 
     #[test]
     fn test_eval_expression_statement() {
         let input = "42; true; 9";
-        let expected = [
-            Object::Integer(42),
-            Object::Boolean(true),
-            Object::Integer(9),
-        ];
-
+        let expected = [Integer(42), Boolean(true), Integer(9)];
         assert_eval(input, &expected);
     }
 
@@ -77,13 +88,21 @@ mod tests {
                 { true; 3; }
             }
         ";
+        let expected = [Integer(5), Boolean(false), Integer(3)];
+        assert_eval(input, &expected);
+    }
 
-        let expected = [
-            Object::Integer(5),
-            Object::Boolean(false),
-            Object::Integer(3),
-        ];
+    #[test]
+    fn test_bang_operator() {
+        let input = "!true; !!false; !!true";
+        let expected = [Boolean(false), Boolean(false), Boolean(true)];
+        assert_eval(input, &expected);
+    }
 
+    #[test]
+    fn test_prefix_minus_operator() {
+        let input = "-5; --42; -0";
+        let expected = [Integer(-5), Integer(42), Integer(0)];
         assert_eval(input, &expected);
     }
 }
